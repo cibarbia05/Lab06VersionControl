8e5479e4dd6bd673b4e4617f38ed28dbc92122f7
Christian Ibarbia RUiz <christianibarbia2015@gmail.com>
2023-10-25 15:48:20 -0400

    Added main function functionality

--- main.py
+++ main.py
@@ -3,4 +3,25 @@ def encoder(password):
 
 
 if __name__ == '__main__':
-    print(encoder('00009962'))
+    print("Menu\n"
+          "-------------\n"
+          "1. Encode\n"
+          "2. Decode\n"
+          "3. Quit")
+
+    option = int(input("\nPlease enter an option: "))
+    encoded_password = ''
+
+    while option != 3:
+        if option == 1:
+            password = input("Please enter your password to encode: ")
+            encoded_password = encoder(password)
+            print("Your password has been encoded and stored!")
+        elif option == 2:
+            decoded_password = decoder(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
+        elif option == 3:
+            break
+        option = int(input("Please enter an option: "))
+
+
