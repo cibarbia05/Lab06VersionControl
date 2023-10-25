fd22ecb53441cb9355bbd0f4aa9ac4931a4b93eb
guzmanJoseph <joseph.guzman05@hotmail.com>
2023-10-25 15:56:28 -0400

    added decoder function and fixed any bugs.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def encoder(password):
+def encoder(password): # Christian Ibarbia Ruiz
     return ''.join([str((int(digit) + 3)) for digit in password])
+
+def decoder(password): # Joseph Guzman
+    decoded_password = ''
+    for i in password:
+        decoded_password += str(int(i) - 3)
+    return decoded_password
 
 
 if __name__ == '__main__':
@@ -16,12 +22,17 @@ if __name__ == '__main__':
         if option == 1:
             password = input("Please enter your password to encode: ")
             encoded_password = encoder(password)
-            print("Your password has been encoded and stored!")
+            print("Your password has been encoded and stored!\n")
         elif option == 2:
             decoded_password = decoder(encoded_password)
-            print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
+            print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.\n")
         elif option == 3:
             break
+        print("Menu\n"
+              "-------------\n"
+              "1. Encode\n"
+              "2. Decode\n"
+              "3. Quit\n")
         option = int(input("Please enter an option: "))
 
 
